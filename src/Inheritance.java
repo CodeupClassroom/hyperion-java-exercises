0bf8b7933f3408bfa8b6a099e94866e0bfbac68b
Casey Friday <casey@friday-next.com>
2020-08-17 09:31:29 -0500

    Inheritance/Polymorphism Lecture Setup

---
+++ src/Inheritance.java
@@ -0,0 +1,17 @@
+// Let's have this 'Inheritance' class extend the 'SuperClass'
+public class Inheritance {
+    // ## Inheritance
+
+    // ## Extending Classes
+
+    public static void main(String[] args) {
+        // ## Access Modifiers
+
+
+
+        // ## The _super_ Keyword
+
+
+    }
+    // Polymorphism -- PolymorphismDemo.java
+}
